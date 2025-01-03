4e4941ac07e4fe0a4d134e359d3a3e4c77042e99
ManavPratapSingh-off <manav3407.gb@gmail.com>
2025-01-03 20:06:33 +0530

    nearing the completion of this project

--- New2U/script.js
+++ New2U/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "9bc65fb1-450f-4794-b74d-c32b7e9b0e1e";
+const API_KEY = "e35d271c2f1e47d48a0a46b6c06fcec2";
 const API_url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener('load', fetchNews("India"));
@@ -7,8 +7,37 @@ async function fetchNews(query) {
     try {
         const fetchedData = await fetch(`${API_url}${query}&apiKey=${API_KEY}`);
         const bundleData = await fetchedData.json();
-        console.log(bundleData);
+        formTemplates(bundleData);
+        
     } catch {
         console.log("some error")
     }
 }
+
+function formTemplates (data) {
+    const mainArea = document.getElementById("main-area");
+    const template = document.getElementById("template");
+    mainArea.innerHTML = "";
+    data.articles.forEach(article => {
+        if (!article.urlToImage) return;
+        let content = template.content.cloneNode(true);
+        fillData(content, article);
+        mainArea.appendChild(content);
+    });
+}
+
+function fillData (content, article) {
+    let img = content.querySelector("#news-card-img");
+    let title = content.querySelector("#news-card-title");
+    let source = content.querySelector("#news-card-src");
+    let desc = content. querySelector("#news-card-news");
+
+    img.src = article.urlToImage;
+    title.innerHTML = article.title;
+    source.innerHTML = article.source.name;
+    desc.innerHTML = article.description;
+    
+}
+
+const linksArray = document.querySelectorAll("li");
+
