108b693de6199419e41c3d640969f1dc7a52d41e
ManavPratapSingh-off <manav3407.gb@gmail.com>
2025-01-03 15:45:34 +0530

    some changes in the script

--- New2U/script.js
+++ New2U/script.js
@@ -1,10 +1,14 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "9bc65fb1-450f-4794-b74d-c32b7e9b0e1e";
 const API_url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener('load', fetchNews("India"));
 
 async function fetchNews(query) {
-    const fetchedData = await fetch(`${API_url}${query}&apiKey=${API_KEY}`);
-    const bundleData = fetchedData.json();
-    console.log(bundleData);
+    try {
+        const fetchedData = await fetch(`${API_url}${query}&apiKey=${API_KEY}`);
+        const bundleData = await fetchedData.json();
+        console.log(bundleData);
+    } catch {
+        console.log("some error")
+    }
 }
