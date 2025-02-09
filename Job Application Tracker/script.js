21fdc59d635cc79b994fee3965a7198cddb3491e
ManavPratapSingh-off <manav3407.gb@gmail.com>
2025-02-09 14:07:23 +0530

    done adding the make_row feature dynamically and some additional css

--- Job Application Tracker/script.js
+++ Job Application Tracker/script.js
@@ -5,6 +5,15 @@ const app_status_input = document.getElementById("status");
 const job_form = document.getElementById("jobForm");
 //till here are all the inputs to be displayed
 
+const applied_jobs_arr = [];
+
+const statusIcons = {
+    "Applied": '<i class="fas fa-file-alt"></i> Applied',
+    "Interviewing": '<i class="fas fa-comments"></i> Interviewing',
+    "Offer-Received": '<i class="fas fa-check-circle"></i> Hired',
+    "Rejected": '<i class="fas fa-times-circle"></i> Rejected'
+};
+
 
 function upload_in_storage () {
     const job_title = job_title_input.value;
@@ -18,9 +27,11 @@ function upload_in_storage () {
         "Application Status" : app_status
     };
 
-    const key = `${job_title}For${company_name}`;
+    applied_jobs_arr.push (ob);
+
+    const key = `applied Jobs`;
            
-    const stored_obj = JSON.stringify(ob);
+    const stored_obj = JSON.stringify(applied_jobs_arr);
   
     localStorage.setItem (key, stored_obj);
 
@@ -31,20 +42,80 @@ function upload_in_storage () {
 
 }
 
+let message_flag = false;
+const modal_overlay = document.getElementById ("modalOverlay");
+const modal = modal_overlay.querySelector(".modal");
+
+function diplay_message () {
+    message_flag = true;
+    modal_overlay.classList.add("active");
+}
+
+function close_popup () {
+    if (message_flag) {
+        
+        modal_overlay.classList.remove("active");
+    }
+    message_flag = false;
+}
+
+const template = document.getElementById ("row-template");
+
+// let row_template = template.content.cloneNode (true);
+// console.log(row_template);
+
 function make_table () {
     //get data from local storage and form rows
+    let table_body = document.querySelector ("tbody");
+    table_body.innerHTML = "";
+    let tmp_job_arr = JSON.parse(localStorage.getItem("applied Jobs"));
+    // console.log (tmp_job_arr);
+    tmp_job_arr.forEach(application => {
+        let tmp_row = template.content.cloneNode (true);
+        
+        let tmp_title = tmp_row.querySelector (".tmpJobTitle");
+        let tmp_company = tmp_row.querySelector (".tmpCompanyName");
+        let tmp_date = tmp_row.querySelector (".tmpAppDate");
+        let tmp_status = tmp_row.querySelector (".tmpAppStatus");
+
+
+        tmp_title.innerText = application["Job Title"];
+        tmp_company.innerText = application["Company Name"];
+        tmp_date.innerText = application["Application Date"];
+        tmp_status.innerHTML = statusIcons [application["Application Status"]];
+        tmp_status.classList.add(application["Application Status"]);
+
+        console.log (tmp_status.classList);
+
+        table_body.appendChild (tmp_row);
+
+
+    });
 
 }
+
 
 function upon_add_application (event) {
     event.preventDefault();
 
     upload_in_storage();
 
+    diplay_message ();
+
+    make_table ();
+
 
 }
 
 job_form.addEventListener ("submit", upon_add_application);
+
+window.addEventListener ("click", function (e) {
+    if (message_flag) {
+        close_popup();
+    }
+})
+
+window.addEventListener ("load", make_table);
 
 
 
